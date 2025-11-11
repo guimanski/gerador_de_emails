f94dc4068525d2bb3bee5e251778d7c4ad5abb71
Guilherme <guiwdomanski@gmail.com>
2025-11-11 19:58:14 -0300

    Exercício de estudo. Criei uma função que processa o nome e o domínio do usuário e os formata no padrão de nome completo e email e retorna uma tupla que é impressa na tela para o usuário.

--- gerador_de_email.py
+++ gerador_de_email.py
@@ -16,8 +16,3 @@ dominio = input('Informe seu dominio (Ex: gmail.com | yahoo.com.br | @gov.br): '
 nome_formatado, email_formatado = formatar_email(nome, dominio)
 
 print(f"Bem-vindo(a) {nome_formatado}. Seu e-mail é: {email_formatado}.")
-
-# pra cara número de 1 até 20, imprima número num passo de 2 em 2
-for numero in range (1, 20, 2):
-    print(numero)
-
